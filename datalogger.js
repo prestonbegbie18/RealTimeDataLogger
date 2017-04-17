36816d2ac71fa0034102016cd4479ae74e855d9c
Unknown <pbegbie@mail.csuchico.edu>
2017-04-17 10:54:21 -0700

    Class Work 4/17/17

---
+++ datalogger.js
@@ -0,0 +1,10 @@
+var numberValues;
+//numberValues = [1, 5, 9, 2];
+
+function addANewNumber()
+{
+  //get text input from the DOM and store it in a variable
+  //check if that variable is a number
+  //if it is, add it to the  back of the array
+  //if it is not, give it a warning or error
+}
